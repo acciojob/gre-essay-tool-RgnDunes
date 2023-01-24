d9f0bfe5fee44bc93021a73ab81b1f599516883b
Divyansh Singh <singh.divyansh1802@gmail.com>
2023-01-24 11:36:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+var textbox = document.getElementById("evaluatedText");
+  var wordCount = document.getElementById("wordCount");
+
+  textbox.addEventListener("input", function() {
+    var text = textbox.value.trim();
+    var count = text.length > 0 ? text.split(" ").length : 0;
+    wordCount.innerHTML = count;
+  });
